b6e8da599a65f2bbea7c5b0caf1065dffb137042
Robin Schroer <sulami@peerwire.org>
2020-05-26 19:37:03 +0200

    Add a first version of the scaffold

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.el
@@ -0,0 +1,35 @@
+;;; {{cookiecutter.project_slug}}.el --- {{cookiecutter.description}} -*- lexical-binding: t -*-
+
+;; Author: {{cookiecutter.author}}
+;; Maintainer: {{cookiecutter.author}}
+;; Version: 0.1
+;; Homepage: https://github.com/{{cookiecutter.username}}/{{cookiecutter.project_slug}}
+;; Package-Requires: ((emacs "25.1")))
+
+
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; {{cookiecutter.description}}
+
+;;; Code:
+
+
+(provide '{{cookiecutter.project_slug}})
+
+;;; {{cookiecutter.project_slug}}.el ends here
